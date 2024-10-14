3847d2dc2f3a1bb0d7c5e95cc7da1ed000c36484
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-10-14 19:56:53 +0400

    Update NovelCard.tsx
    Updated the novel card component, now displaying link to that novels page.

--- src/Components/NovelCard.tsx
+++ src/Components/NovelCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { NovelDTO } from "../Types/types";
 
 const NovelCard: React.FC<NovelDTO> = ({ Title, Author, Novel, onHover }) => {
@@ -20,7 +21,15 @@ const NovelCard: React.FC<NovelDTO> = ({ Title, Author, Novel, onHover }) => {
             className="my-3 py-2 border-b border-zinc-800"
         >
             <h2 className="text-2xl">{Title}</h2>
-            <h2 className="text-1xl">{Author}</h2>
+            <h2 className="mx-1 mt-1 flex justify-between">
+                <div className="text-1xl">
+                    {" "}
+                    {" > "} {Author}
+                </div>
+                <Link to={`/novel/${Title}`} className="mx-1 link">
+                    [Read]
+                </Link>
+            </h2>
         </div>
     );
 };
