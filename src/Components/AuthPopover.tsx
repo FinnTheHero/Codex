fbb223bf7fe28a8e05572852b204d8c0ee362b8f
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-11-08 18:15:21 +0400

    Create AuthPopover.tsx
    Created AuthPopover component for user login/signup access.

---
+++ src/Components/AuthPopover.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthPopoverProps } from "../Types/types";
+
+const AuthPopover: React.FC<AuthPopoverProps> = ({ children }) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isChildHovered, setIsChildHovered] = useState(false);
+
+    useEffect(() => {
+        if (isHovered) {
+            let time = 1500;
+            if (isChildHovered) {
+                time = 999999;
+            }
+            const timer = setTimeout(() => {
+                setIsHovered(false);
+            }, time);
+
+            return () => clearTimeout(timer);
+        }
+    }, [isHovered, isChildHovered]);
+
+    return (
+        <div
+            style={{ position: "relative", display: "inline-block" }}
+            onMouseEnter={() => setIsHovered(true)}
+            onClick={() => setIsHovered(true)}
+        >
+            {children}
+            {isHovered && (
+                <div
+                    className={`text-lg content popover-bottom flex flex-row flex-wrap text-center ${isHovered ? "popover-visible" : ""} mb-1 px-1 py-2 border border-zinc-800 rounded`}
+                    style={{
+                        whiteSpace: "nowrap",
+                    }}
+                    onMouseEnter={() => setIsChildHovered(true)}
+                    onMouseLeave={() => setIsChildHovered(false)}
+                >
+                    <Link className="mx-auto mb-1" to={"/login"}>
+                        [Login]
+                    </Link>
+                    <Link to={"/Signup"}>[Signup]</Link>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AuthPopover;
