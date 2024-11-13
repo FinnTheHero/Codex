63fd431b91181ef3352501da31a8e24435f5f65f
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-11-13 15:06:29 +0400

    Update ErrorAlert.tsx
    Updated styles

--- src/Components/ErrorAlert.tsx
+++ src/Components/ErrorAlert.tsx
@@ -2,9 +2,12 @@ const ErrorAlert = ({ error }: { error: string }) => {
     return (
         <div
             id="toast-top-right"
-            className="custom-alert fixed flex items-center w-full max-w-xs p-4 space-x-4 divide-x rtl:divide-x-reverse rounded-lg top-5 right-5"
+            className="custom-alert fixed flex items-center w-full max-w-fit p-4 rtl:divide-x-reverse rounded-lg top-20 right-1/2 translate-x-1/2 lg:top-5 lg:right-5 lg:translate-x-0"
             role="alert"
-            style={{ color: "red", borderColor: "red" }}
+            style={{
+                color: "red",
+                borderColor: "red",
+            }}
         >
             <div>{error}</div>
         </div>
