ac6d55a16fa5ef6ef5b1c513bf740008b966a88d
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-11-29 21:38:29 +0400

    Added markdown support

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,7 +1,13 @@
+import ReactMarkdown from "react-markdown";
+
 const Dashboard = () => {
+    const markdownText = `# React Markdown Example\n\n- Some text\n- Some other text\n## Subtitle\n### Additional info\nThis is a [link](https://github.com/remarkjs/react-markdown)`;
+
     return (
         <div>
-            <h2>Dashboard</h2>
+            <ReactMarkdown className="prose markdown">
+                {markdownText}
+            </ReactMarkdown>
         </div>
     );
 };
