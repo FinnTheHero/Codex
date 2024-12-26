20844e721cbdc17f804e9889e42c41c5bd598bf8
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-12-26 23:27:27 +0400

    Cleaned up dashboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,13 +1,14 @@
-import ReactMarkdown from "react-markdown";
+// import ReactMarkdown from "react-markdown";
 
 const Dashboard = () => {
-    const markdownText = `# React Markdown Example\n\n- Some text\n- Some other text\n## Subtitle\n### Additional info\nThis is a [link](https://github.com/remarkjs/react-markdown)`;
-
     return (
         <div>
-            <ReactMarkdown className="prose markdown">
-                {markdownText}
-            </ReactMarkdown>
+            {/*
+                <ReactMarkdown className="prose markdown">
+                    {markdownText}
+                </ReactMarkdown>
+            */}
+            <p>This is dashboard page</p>
         </div>
     );
 };
