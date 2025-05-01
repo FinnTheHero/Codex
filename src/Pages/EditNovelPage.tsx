4cf2fb3a9084164bd5546c6b24d77e7cd633f666
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-05-01 14:26:47 +0200

    Update EditNovelPage.tsx
    Modified design slightly.

--- src/Pages/EditNovelPage.tsx
+++ src/Pages/EditNovelPage.tsx
@@ -64,51 +64,57 @@ const EditNovelPage = () => {
     }, [handleNovelSearch]);
 
     return (
-        <div className="max-w-4xl w-full px-8 lg:p-12 flex flex-row flex-wrap justify-center align-middle">
-            <div className="w-full flex justify-center mb-12 lg:mb-0 pr-6">
+        <div className="max-w-4xl w-full h-full px-8 lg:px-12 flex flex-row flex-wrap">
+            <div className="w-full h-full flex justify-center mb-12 lg:mb-0">
                 {novel && (
-                    <form className="flex flex-col items-start flex-nowrap w-full">
-                        <label className="w-full flex justify-center text-xl">
+                    <form className="flex flex-col items-start justify-between flex-nowrap w-full h-full">
+                        <label className="w-full mb-16 flex justify-center text-xl">
                             Currently Editing
                             <h2 className="mx-2 content">[Novel]</h2>
                             <h2 className="link">[{novel.title}]</h2>
                         </label>
 
-                        <div className="my-12 flex flex-col">
-                            <p className="text-sm content">
-                                Can't change the author!
-                            </p>
-                            <label className="text-xl font-bold">Author</label>
-                            <label className="text-lg mx-2">
-                                {novel.author}
-                            </label>
-                        </div>
+                        <div className="w-full my-14 flex flex-row flex-nowrap justify-between items-center">
+                            <div className="flex flex-col">
+                                <p className="text-sm content">
+                                    Can't change the author!
+                                </p>
+                                <label className="text-xl font-bold">
+                                    Author
+                                </label>
+                                <label className="text-lg mx-2">
+                                    {novel.author}
+                                </label>
+                            </div>
 
-                        <div className="mb-12 flex flex-col flex-nowrap">
-                            <label className="text-xl font-bold">Title</label>
-                            <div className="flex flex-row flex-nowrap mx-2">
-                                <div className="text-lg flex justify-center items-center">
-                                    <label>{novel.title}</label>
-                                </div>
+                            <div className="flex flex-col flex-nowrap">
+                                <label className="text-xl font-bold">
+                                    Title
+                                </label>
+                                <div className="flex flex-row flex-nowrap mx-2">
+                                    <div className="text-lg flex justify-center items-center">
+                                        <label>{novel.title}</label>
+                                    </div>
 
-                                <h2 className="mx-4 flex items-center">
-                                    <FontAwesomeIcon
-                                        size="xs"
-                                        icon={faArrowRight}
-                                    />
-                                </h2>
+                                    <h2 className="mx-4 flex items-center">
+                                        <FontAwesomeIcon
+                                            size="xs"
+                                            icon={faArrowRight}
+                                        />
+                                    </h2>
 
-                                <div className="border-b border-zinc-800 w-fit">
-                                    <input
-                                        id="title"
-                                        name="title"
-                                        type="text"
-                                        className="search-input mb-1"
-                                        onChange={handleTitleChange}
-                                        placeholder={String(
-                                            "New " + novel.title,
-                                        )}
-                                    />
+                                    <div className="border-b border-zinc-800 w-fit">
+                                        <input
+                                            id="title"
+                                            name="title"
+                                            type="text"
+                                            className="search-input mb-1"
+                                            onChange={handleTitleChange}
+                                            placeholder={String(
+                                                "New " + novel.title,
+                                            )}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </div>
