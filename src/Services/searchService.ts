843989289b23980a8e2fec6bd41a31d0aad17ec8
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-21 17:44:17 +0400

    Updated code for searching novels and chapters.

--- src/Services/searchService.ts
+++ src/Services/searchService.ts
@@ -1,44 +1,62 @@
 import axios from "axios";
 import api from "./apiService";
 
+const handleErr = (err: any) => {
+    if (axios.isAxiosError(err)) {
+        throw new Error(err.response?.statusText || "Unknown Error");
+    } else {
+        throw new Error("Unknown Error");
+    }
+};
+
 export const searchChapter = async (novel: string, chapter: string) => {
     try {
         if (novel === undefined || chapter === undefined) {
             throw new Error("Novel or chapter title not provided!");
         }
 
-        if (chapter === "") {
-            chapter = "all";
+        if (novel === "" || chapter === "") {
+            throw new Error("Novel or chapter title not provided!");
         }
 
         const response = await api.get(`/${novel}/${chapter}`);
         return response.data;
     } catch (err) {
-        if (axios.isAxiosError(err)) {
-            throw new Error(err.response?.statusText || "Unknown Error");
-        } else {
-            throw new Error("Unknown Error");
+        handleErr(err);
+    }
+};
+
+export const searchAllChapters = async (novel: string) => {
+    try {
+        if (novel === undefined || novel === "") {
+            throw new Error("Novel title not provided!");
         }
+
+        const response = await api.get(`/${novel}/all`);
+        return response.data;
+    } catch (err) {
+        handleErr(err);
     }
 };
 
 export const searchNovel = async (novel: string) => {
     try {
-        if (novel === undefined) {
+        if (novel === undefined || novel === "") {
             throw new Error("Novel title not provided!");
-        }
-
-        if (novel === "") {
-            novel = "all";
         }
 
         const response = await api.get(`/${novel}`);
         return response.data;
     } catch (err) {
-        if (axios.isAxiosError(err)) {
-            throw new Error(err.response?.statusText || "Unknown Error");
-        } else {
-            throw new Error("Unknown Error");
-        }
+        handleErr(err);
+    }
+};
+
+export const searchAllNovels = async () => {
+    try {
+        const response = await api.get(`/all`);
+        return response.data;
+    } catch (err) {
+        handleErr(err);
     }
 };
