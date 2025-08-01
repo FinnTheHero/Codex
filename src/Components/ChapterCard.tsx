bf680bc6a058ff85ea35be06ef56edc4af346303
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-01 20:32:27 +0400

    Updated Card Components to use Content Context.

--- src/Components/ChapterCard.tsx
+++ src/Components/ChapterCard.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ChapterCardProps } from "../Types/types";
+import { ChapterCardProps, NovelCardProps } from "../Types/types";
 import FormattedTime from "./FormattedTime";
+import useSWR from "swr";
+import { useContent } from "../Contexts/ContentContext";
 
-const ChapterCard: React.FC<ChapterCardProps> = ({ chapter, novel }) => {
+const ChapterCard: React.FC<NovelCardProps> = ({ index }) => {
+    const { novel, chapter, setChapter, chapters } = useContent();
+
+    if (!novel) return null;
+
+    if (!chapter && chapters.length >= 0) {
+        setChapter(chapters[index]);
+    }
+
     return (
         <div className="my-3 py-2 border-b border-zinc-800">
-            <h2 className="text-2xl">{chapter.title}</h2>
-            <div className="mx-1 mt-1 flex justify-between">
-                <FormattedTime
-                    date={chapter.upload_date}
-                    classname={"link"}
-                    popover_text={"Uploaded"}
-                />
-                <Link
-                    to={`/novels/${novel.id}/${chapter.id}`}
-                    className="mx-1 link "
-                >
-                    [Read]
-                </Link>
-            </div>
+            {chapter && (
+                <div>
+                    <h2 className="text-2xl">{chapter.title}</h2>
+                    <div className="mx-1 mt-1 flex justify-between">
+                        <FormattedTime
+                            date={chapter.upload_date}
+                            classname={"link"}
+                            popover_text={"Uploaded"}
+                        />
+                        <Link
+                            to={`/novels/${novel.id}/${chapter.id}`}
+                            className="mx-1 link "
+                        >
+                            [Read]
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
--- src/Components/NovelCard.tsx
+++ src/Components/NovelCard.tsx
@@ -2,49 +2,30 @@ import { useCallback } from "react";
 import { Link } from "react-router-dom";
 import { useError } from "../Contexts/ErrorContext";
 import { useLoading } from "../Contexts/LoadingContext";
-import { searchAllChapters, searchChapter } from "../Services/searchService";
 import { Chapter, NovelCardProps } from "../Types/types";
 import { useSearchHandler } from "./SearchHandler";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import { useContent } from "../Contexts/ContentContext";
 
 const NovelCard: React.FC<NovelCardProps> = ({ index }) => {
     let hoverTimeout: ReturnType<typeof setTimeout>;
 
-    const { novel, novels, setNovel, setChapters } = useContent();
+    const { novel, novels, setNovel } = useContent();
 
     setNovel(novels[index]);
 
-    const { data, error } = useSWR<any>(novel ? `/${novel.id}/all` : null);
     const { setLoading } = useLoading();
     const { addError } = useError();
 
-    const handleAllChaptersSearch = async () => {
-        setLoading(true);
-        setChapters([]);
-
-        try {
-            if (data.chapters && data.chapters.length > 0) {
-                setNovel(novel);
-                setChapters(data.chapters as Chapter[]);
-                return;
-            }
-        } catch (err) {
-            addError((err as Error).message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
     const handleMouseEnter = () => {
-        hoverTimeout = setTimeout(() => {
-            handleAllChaptersSearch();
-        }, 1000);
+        hoverTimeout = setTimeout(() => {}, 1000);
     };
 
     const handleMouseLeave = () => {
         clearTimeout(hoverTimeout);
     };
+
+    console.log(novel, novels);
 
     return (
         <div
@@ -52,14 +33,14 @@ const NovelCard: React.FC<NovelCardProps> = ({ index }) => {
             onMouseLeave={handleMouseLeave}
             className="my-3 py-2 border-b border-zinc-800"
         >
-            {novels && (
+            {novel && (
                 <div>
                     <h2 className="text-2xl">{novel?.title}</h2>
                     <div className="mx-1 mt-1 flex justify-between">
                         <div className="subtitle">
-                            {" > "} {novel?.author}
+                            {" > "} {novel.author}
                         </div>
-                        <Link to={`/novels/${novel?.id}`} className="mx-1 link">
+                        <Link to={`/novels/${novel.id}`} className="mx-1 link">
                             [Read]
                         </Link>
                     </div>
