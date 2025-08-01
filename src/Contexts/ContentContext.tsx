28c60f8c94ce59ca45bbb2538d4b9ce8cede627f
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-01 20:30:50 +0400

    Implemented SWR directly into context.
    Added SWR background fetching directly in the context to minimize the API calls, globalize novel/chapter access.
    Added re-fetching novels/chapters on demand with exported functions.

--- src/Contexts/ContentContext.tsx
+++ src/Contexts/ContentContext.tsx
@@ -3,6 +3,9 @@ import { Chapter, ContentContextType, Novel } from "../Types/types";
 import { useError } from "./ErrorContext";
 import { useLoading } from "./LoadingContext";
 import { useNotification } from "./NotificationContext";
+import useSWR from "swr";
+import { axiosFetcher } from "../Services/apiService";
+import { useEffect } from "react";
 
 const ContentContext = createContext<ContentContextType | undefined>(undefined);
 
@@ -13,23 +16,54 @@ export const ContentProvider: React.FC<{ children: ReactNode }> = ({
     const { setLoading } = useLoading();
     const { setNotification } = useNotification();
 
-    const [chapters, setChapters] = useState<Chapter[]>([]);
-    const [novels, setNovels] = useState<Novel[]>([]);
-
     const [chapter, setChapter] = useState<Chapter | null>(null);
     const [novel, setNovel] = useState<Novel | null>(null);
+
+    const key_n = `/all`;
+    const key_c = novel ? `/${novel.id}/all` : null;
+
+    const {
+        data: data_n,
+        error: error_n,
+        mutate: mutateNovels,
+        isLoading: novelsLoading,
+    } = useSWR<{ novels: Novel[] }>(key_n);
+
+    const {
+        data: data_c,
+        error: error_c,
+        mutate: mutateChapters,
+        isLoading: chaptersLoading,
+    } = useSWR<{ chapters: Chapter[] }>(novel ? key_c : null);
+
+    useEffect(() => {
+        setLoading(novelsLoading || chaptersLoading);
+    }, [novelsLoading, chaptersLoading]);
+
+    useEffect(() => {
+        if (error_n) addError(error_n);
+        if (error_c) addError(error_c);
+    }, [error_n, error_c]);
+
+    async function refreshAllChapters() {
+        mutateChapters();
+    }
+
+    async function refreshAllNovels() {
+        mutateNovels();
+    }
 
     return (
         <ContentContext.Provider
             value={{
                 chapter,
                 setChapter,
-                chapters,
-                setChapters,
-                novels,
-                setNovels,
+                chapters: data_c?.chapters ?? [],
+                novels: data_n?.novels ?? [],
                 novel,
                 setNovel,
+                refreshAllChapters,
+                refreshAllNovels,
             }}
         >
             {children}
