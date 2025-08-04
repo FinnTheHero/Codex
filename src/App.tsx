9c0d7867c8e4fa6ec9e92028109d7b1c2175558c
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-05 00:12:06 +0400

    Added path for upload epub page.

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ import {
 import EditNovelPage from "./Pages/EditNovelPage";
 import EditChapterPage from "./Pages/EditChapterPage";
 import { PageAnimationWrapper } from "./Components/PageAnimationWrapper";
+import UploadEPUBPage from "./Pages/UploadEPUBPage";
 
 function App() {
     return (
@@ -168,6 +169,17 @@ const RouterTransition = () => {
                             <PageAnimationWrapper>
                                 <RequireUser>
                                     <UploadPage />
+                                </RequireUser>
+                            </PageAnimationWrapper>
+                        }
+                    />
+
+                    <Route
+                        path="/dashboard/upload/epub"
+                        element={
+                            <PageAnimationWrapper>
+                                <RequireUser>
+                                    <UploadEPUBPage />
                                 </RequireUser>
                             </PageAnimationWrapper>
                         }
