f547b6c99297ad3d62a2c6980c1fce4351c142d1
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-05 16:05:19 +0400

    Removed extra code and updated chapter card component.

--- src/Components/ChapterCard.tsx
+++ src/Components/ChapterCard.tsx
@@ -6,27 +6,23 @@ import useSWR from "swr";
 import { useContent } from "../Contexts/ContentContext";
 
 const ChapterCard: React.FC<NovelCardProps> = ({ index }) => {
-    const { novel, chapter, setChapter, chapters } = useContent();
+    const { novel, chapters } = useContent();
 
     if (!novel) return null;
 
-    if (!chapter && chapters.length > 0) {
-        setChapter(chapters[index]);
-    }
-
     return (
         <div className="my-3 py-2 border-b border-zinc-800">
-            {chapter && (
+            {chapters[index] && (
                 <div>
-                    <h2 className="text-2xl">{chapter.title}</h2>
+                    <h2 className="text-2xl">{chapters[index].title}</h2>
                     <div className="mx-1 mt-1 flex justify-between">
                         <FormattedTime
-                            date={chapter.upload_date}
+                            date={chapters[index].upload_date}
                             classname={"link"}
                             popover_text={"Uploaded"}
                         />
                         <Link
-                            to={`/novels/${novel.id}/${chapter.id}`}
+                            to={`/novels/${novel.id}/${chapters[index].id}`}
                             className="mx-1 link "
                         >
                             [Read]
