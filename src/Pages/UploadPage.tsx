da882d7ef56318edf065ac764f310f211102922e
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-05 16:00:13 +0400

    Fixed link paths to upload pages.

--- src/Pages/UploadPage.tsx
+++ src/Pages/UploadPage.tsx
@@ -5,10 +5,10 @@ const UploadPage = () => {
         <div className="max-w-6xl w-full h-full px-8 lg:px-12 flex flex-row flex-wrap">
             <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center">
                 <div className="w-1/2 h-1/3 link text-2xl flex flex-col flex-nowrap text-nowrap items-center text-center justify-evenly px-6 border-r-2 border-zinc-800">
-                    <Link to="/upload/novel" className="link">
+                    <Link to="/dashboard/upload/novel" className="link">
                         [Upload Novel]
                     </Link>
-                    <Link to="/upload/chapter" className="link">
+                    <Link to="/dashboard/upload/chapter" className="link">
                         [Upload Chapter]
                     </Link>
                 </div>
