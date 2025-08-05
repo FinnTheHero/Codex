63d30bd7fcbddbf63d87a5c36b83fd3176893951
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-05 21:18:19 +0400

    Big update.
    Implemented EPUB parsing.
    User can no upload EPUB file, page will parse it using epubjs library, load the content, export chapters in HTML format, parse and remove unwanted tags, convert content to Markdown, create novel, upload novel, get the new novel ID.
    Upcoming commit will include batch uploading prepared markdown chapters.

--- src/Pages/UploadEPUBPage.tsx
+++ src/Pages/UploadEPUBPage.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
 import ePub, { Book } from "epubjs";
+import { useError } from "../Contexts/ErrorContext";
+import { useNotification } from "../Contexts/NotificationContext";
+import { useLoading } from "../Contexts/LoadingContext";
+import { Chapter, Novel } from "../Types/types";
+import axios from "axios";
+import api from "../Services/apiService";
+import { createNovel } from "../Services/createService";
+import { useContent } from "../Contexts/ContentContext";
+import { HandleErr } from "../Services/errorHandler";
+import TurndownService from "turndown";
 
 const UploadEPUBPage = () => {
     const [file, setFile] = useState<File | null>(null);
-    const [processing, setProcessing] = useState<boolean>(false);
+
+    const { addError } = useError();
+    const { setLoading } = useLoading();
+    const { setNotification } = useNotification();
+    const { novels, refreshAllNovels } = useContent();
+
+    // HTML to Markdown conversion rules - Requires further testing and feedback
+    const turndown = new TurndownService({
+        headingStyle: "atx",
+        codeBlockStyle: "fenced",
+        bulletListMarker: "-",
+    });
+    turndown.remove(["img", "script", "style", "meta"]);
+    turndown.addRule("skipSynopsisParas", {
+        filter: (node) =>
+            node.nodeName === "P" && / TL /.test(node.textContent || ""),
+        replacement: () => "",
+    });
 
     const handleFileChange = async (
         event: React.ChangeEvent<HTMLInputElement>,
@@ -12,9 +39,72 @@ const UploadEPUBPage = () => {
             return;
         }
         setFile(event.target.files[0]);
-        setProcessing(true);
+        setNotification("File Set");
+    };
 
-        // Continue here tomorrow
+    const handleUpload = async () => {
+        if (!file) {
+            setLoading(false);
+            return addError("No file selected");
+        }
+
+        setNotification(`Processing ${file.name}`);
+        setLoading(true);
+
+        const arrayBuffer = await file.arrayBuffer();
+        const book: Book = ePub(arrayBuffer);
+
+        try {
+            await book.ready;
+            await book.loaded.spine;
+        } catch (error) {
+            setLoading(false);
+            return addError("Error opening file");
+        }
+
+        const metadata = await book.loaded.metadata;
+        const navigation = await book.loaded.navigation;
+
+        const chapters: Chapter[] = [];
+        book.spine.each(async (section: any) => {
+            await section.load(book.load.bind(book));
+            const html = await section.render();
+            console.log(html);
+            const markdown = turndown.turndown(html);
+            console.log(markdown);
+            await section.unload();
+        });
+
+        console.log(navigation);
+        console.log(book);
+        console.log(metadata);
+
+        setNotification("Creating novel...");
+
+        const novel: Novel = {
+            id: "",
+            title: metadata.title || "Untitled",
+            author: metadata.creator || "Unknown",
+            description: metadata.description || "",
+            creation_date: "",
+            update_date: "",
+            upload_date: "",
+        };
+
+        const response = await createNovel(novel);
+        setNotification(response);
+        setLoading(false);
+
+        setNotification("Fetching Novel ID...");
+        setLoading(true);
+
+        await refreshAllNovels();
+        const id = novels.find((novel) => novel.title === metadata.title)?.id;
+
+        if (!id) {
+            setLoading(false);
+            return addError("Novel not found");
+        }
     };
 
     return (
@@ -22,7 +112,25 @@ const UploadEPUBPage = () => {
             <p className="content">
                 Upload EPUB file to parse Novel and Chapters directly
             </p>
-            <p className="link">[Pick EPUB file]</p>
+            <div className="flex flex-col flex-nowrap justify-center items-center">
+                <label
+                    htmlFor="epub-picker"
+                    className="link cursor-pointer mb-6"
+                >
+                    [Pick EPUB File]
+                </label>
+                <input
+                    id="epub-picker"
+                    className="hidden"
+                    type="file"
+                    accept=".epub"
+                    onChange={handleFileChange}
+                />
+                <label className="">{file?.name}</label>
+            </div>
+            <button className="link" onClick={handleUpload}>
+                [Upload]
+            </button>
         </div>
     );
 };
