51136667c2d5fb5b38951ec00cca470fc13fc10a
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-05 15:58:04 +0400

    FIxed hero page for updated hero page layout.

--- src/Pages/HeroPage.tsx
+++ src/Pages/HeroPage.tsx
@@ -1,7 +1,7 @@
 const HeroPage = () => {
     return (
         <div className="text-center text-4xl">
-            <div className="w-full px-12 text-center">
+            <div className="w-full h-full px-12 text-center flex flex-col items-center justify-center">
                 <h1 className="text-2xl lg:text-3xl">Welcome to Codex</h1>
                 <p className="subtitle text-xl lg:text-2xl">
                     A novel reading platform - read anytime, anywhere.
