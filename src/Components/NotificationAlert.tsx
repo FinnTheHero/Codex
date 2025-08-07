283fe4b744debe3b2927361b9d8057147ff500da
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-07 23:47:09 +0400

    Updated notification styles.

--- src/Components/NotificationAlert.tsx
+++ src/Components/NotificationAlert.tsx
@@ -1,7 +1,7 @@
 const NotificationAlert = ({ notification }: { notification: string }) => {
     return (
         <div
-            className="custom-alert content flex items-center w-full max-w-fit p-4 my-2 rounded-lg"
+            className="custom-alert content flex items-center text-center max-w-fit w-fit px-4 py-2 rounded-lg"
             style={{
                 color: "#89dceb",
                 borderColor: "#89dceb",
