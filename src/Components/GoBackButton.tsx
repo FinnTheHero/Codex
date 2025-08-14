bccada951b51153a0043c64f7329bfb74a466f9b
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-14 20:48:00 +0400

    feat: Refactor GoBackButton with descriptive hover popover

--- src/Components/GoBackButton.tsx
+++ src/Components/GoBackButton.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Popover } from "react-tiny-popover";
 
-const GoBackButton = ({ to, className }: { to: string; className: string }) => {
+const GoBackButton = ({
+    to,
+    className,
+    desc,
+}: {
+    to: string;
+    className: string;
+    desc: string;
+}) => {
+    const [isBackPopoverOpen, setIsBackPopoverOpen] = useState(false);
+
     return (
-        <Link to={to} className={className}>
-            [Go Back]
-        </Link>
+        <Popover
+            isOpen={isBackPopoverOpen && desc !== ""}
+            positions={["bottom", "left", "top", "right"]}
+            padding={10}
+            reposition={true}
+            boundaryInset={document.body.scrollHeight}
+            onClickOutside={() => setIsBackPopoverOpen(false)}
+            content={
+                <div className="link main-background whitespace-nowrap p-2 border border-zinc-800 rounded text-lg">
+                    [{desc}]
+                </div>
+            }
+        >
+            <Link
+                to={to}
+                className={className}
+                onMouseEnter={() => setIsBackPopoverOpen(true)}
+                onMouseLeave={() => setIsBackPopoverOpen(false)}
+            >
+                [Go Back]
+            </Link>
+        </Popover>
     );
 };
 
