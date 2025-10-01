abd2e37c283856047f3a42173078a4939e60b7bd
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-10-01 22:10:55 +0400

    feat: Improve UploadPage layout and add navigation

--- src/Pages/UploadPage.tsx
+++ src/Pages/UploadPage.tsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
+import GoBackButton from "../Components/GoBackButton";
 
 const UploadPage = () => {
     return (
-        <div className="max-w-6xl w-full h-full px-8 lg:px-12 flex flex-row flex-wrap">
-            <div className="w-full h-full flex flex-row flex-nowrap justify-center items-center">
-                <div className="w-1/2 h-1/3 link text-2xl flex flex-col flex-nowrap text-nowrap items-center text-center justify-evenly px-6 border-r-2 border-zinc-800">
+        <div className="max-w-6xl w-full h-full px-8 lg:px-12 flex flex-col flex-nowrap items-center justify-evenly">
+            <div className="w-full h-auto text-2xl flex flex-col md:flex-row flex-wrap justify-evenly items-center">
+                <div className="w-auto flex flex-col items-center justify-between">
                     <Link to="/dashboard/upload/novel" className="link">
                         [Upload Novel]
                     </Link>
-                    <Link to="/dashboard/upload/chapter" className="link">
+                    <Link to="/dashboard/upload/chapter" className="link mt-6">
                         [Upload Chapter]
                     </Link>
                 </div>
-                <div className="h-1/3 flex flex-col flex-nowrap items-center justify-evenly text-center text-2xl ml-6">
-                    <p className="content">
-                        Upload EPUB file to parse Novel and Chapters directly
-                    </p>
+                <p className="content my-12">Or</p>
+                <div className="w-auto flex flex-col flex-nowrap items-center justify-evenly">
                     <Link to="/dashboard/upload/epub" className="link">
                         [Pick EPUB file]
                     </Link>
                 </div>
             </div>
+            <GoBackButton
+                className="link mt-24 text-xl"
+                to={`/novels`}
+                desc={`Novels`}
+            />
         </div>
     );
 };
